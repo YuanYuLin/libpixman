ee272ef4c2fc918d0908f01e16b6e5fc919ed117
yyl <yuanyu.lin@gmail.com>
2018-03-28 10:19:49 +0800

    MAJOR_NUM=1 MINOR_NUM=3 AUX_NUM=0 update package

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -1,72 +1,62 @@
 import ops
 import iopc
 
+TARBALL_FILE="pixman-0.34.0.tar.gz"
+TARBALL_DIR="pixman-0.34.0"
+INSTALL_DIR="pixman-bin"
 pkg_path = ""
 output_dir = ""
-arch = ""
-src_usr_lib_dir = ""
-dst_usr_lib_dir = ""
-src_lib_dir = ""
-dst_lib_dir = ""
-src_include_dir = ""
+tarball_pkg = ""
+tarball_dir = ""
+install_dir = ""
+install_tmp_dir = ""
+cc_host = ""
 dst_include_dir = ""
+dst_lib_dir = ""
 
 def set_global(args):
     global pkg_path
     global output_dir
-    global arch
-    global src_usr_lib_dir
-    global dst_usr_lib_dir
-    global src_lib_dir
-    global dst_lib_dir
-    global src_include_dir
+    global tarball_pkg
+    global install_dir
+    global install_tmp_dir
+    global tarball_dir
+    global cc_host
     global dst_include_dir
+    global dst_lib_dir
     pkg_path = args["pkg_path"]
     output_dir = args["output_path"]
-    arch = ops.getEnv("ARCH_ALT")
-    if arch == "armhf":
-        src_usr_lib_dir = iopc.getBaseRootFile("usr/lib/arm-linux-gnueabihf")
-        src_lib_dir = iopc.getBaseRootFile("lib/arm-linux-gnueabihf")
-    elif arch == "armel":
-        src_usr_lib_dir = iopc.getBaseRootFile("usr/lib/arm-linux-gnueabi")
-        src_lib_dir = iopc.getBaseRootFile("lib/arm-linux-gnueabi")
-    elif arch == "x86_64":
-        src_usr_lib_dir = iopc.getBaseRootFile("usr/lib/x86_64-linux-gnu")
-        src_lib_dir = iopc.getBaseRootFile("lib/x86_64-linux-gnu")
-    else:
-        sys.exit(1)
-    dst_usr_lib_dir = ops.path_join(output_dir, "usr/lib")
-    dst_lib_dir = ops.path_join(output_dir, "lib")
-
-    src_include_dir = iopc.getBaseRootFile("usr/include")
+    tarball_pkg = ops.path_join(pkg_path, TARBALL_FILE)
+    install_dir = ops.path_join(output_dir, INSTALL_DIR)
+    install_tmp_dir = ops.path_join(output_dir, INSTALL_DIR + "-tmp")
+    tarball_dir = ops.path_join(output_dir, TARBALL_DIR)
+    cc_host_str = ops.getEnv("CROSS_COMPILE")
+    cc_host = cc_host_str[:len(cc_host_str) - 1]
     dst_include_dir = ops.path_join("include",args["pkg_name"])
-
+    dst_lib_dir = ops.path_join(install_dir, "lib")
 
 def MAIN_ENV(args):
     set_global(args)
+
+    ops.exportEnv(ops.setEnv("CC", ops.getEnv("CROSS_COMPILE") + "gcc"))
+    ops.exportEnv(ops.setEnv("CXX", ops.getEnv("CROSS_COMPILE") + "g++"))
+    ops.exportEnv(ops.setEnv("CROSS", ops.getEnv("CROSS_COMPILE")))
+    ops.exportEnv(ops.setEnv("DESTDIR", install_tmp_dir))
+
     return False
 
 def MAIN_EXTRACT(args):
     set_global(args)
 
-    ops.mkdir(dst_usr_lib_dir)
-    ops.copyto(ops.path_join(src_usr_lib_dir, "libpixman-1.so.0.34.0"), dst_usr_lib_dir)
-    ops.ln(dst_usr_lib_dir, "libpixman-1.so.0.34.0", "libpixman-1.so.0.34")
-    ops.ln(dst_usr_lib_dir, "libpixman-1.so.0.34.0", "libpixman-1.so.0")
-    ops.ln(dst_usr_lib_dir, "libpixman-1.so.0.34.0", "libpixman-1.so")
-
-    ops.mkdir(dst_lib_dir)
-    ops.copyto(ops.path_join(src_lib_dir, "libcap-ng.so.0.0.0"), dst_lib_dir)
-    ops.ln(dst_lib_dir, "libcap-ng.so.0.0.0", "libcap-ng.so.0.0")
-    ops.ln(dst_lib_dir, "libcap-ng.so.0.0.0", "libcap-ng.so.0")
-    ops.ln(dst_lib_dir, "libcap-ng.so.0.0.0", "libcap-ng.so")
+    ops.unTarGz(tarball_pkg, output_dir)
+    #ops.copyto(ops.path_join(pkg_path, "finit.conf"), output_dir)
 
     return True
 
 def MAIN_PATCH(args, patch_group_name):
     set_global(args)
     for patch in iopc.get_patch_list(pkg_path, patch_group_name):
-        if iopc.apply_patch(build_dir, patch):
+        if iopc.apply_patch(tarball_dir, patch):
             continue
         else:
             sys.exit(1)
@@ -75,20 +65,45 @@ def MAIN_PATCH(args, patch_group_name):
 
 def MAIN_CONFIGURE(args):
     set_global(args)
-    return False
+
+    extra_conf = []
+    extra_conf.append("--host=" + cc_host)
+    iopc.configure(tarball_dir, extra_conf)
+
+    return True
 
 def MAIN_BUILD(args):
     set_global(args)
+
+    ops.mkdir(install_dir)
+    ops.mkdir(install_tmp_dir)
+    iopc.make(tarball_dir)
+    iopc.make_install(tarball_dir)
     return False
 
 def MAIN_INSTALL(args):
     set_global(args)
 
-    iopc.installBin(args["pkg_name"], ops.path_join(dst_usr_lib_dir, "."), "usr/lib") 
+    ops.mkdir(install_dir)
+
+    ops.mkdir(dst_lib_dir)
+    libpixman = "libpixman-1.so.0.34.0"
+    ops.copyto(ops.path_join(install_tmp_dir, "usr/local/lib/" + libpixman), dst_lib_dir)
+    ops.ln(dst_lib_dir, libpixman, "libpixman-1.so.0")
+    ops.ln(dst_lib_dir, libpixman, "libpixman-1.so")
+
+    ops.mkdir(dst_include_dir)
+    ops.copyto(ops.path_join(install_tmp_dir, "usr/local/include/pixman-1/pixman.h"), dst_include_dir)
+    ops.copyto(ops.path_join(install_tmp_dir, "usr/local/include/pixman-1/pixman-version.h"), dst_include_dir)
+
+    iopc.installBin(args["pkg_name"], ops.path_join(ops.path_join(install_dir, "lib"), "."), "lib")
+    iopc.installBin(args["pkg_name"], dst_include_dir, "include")
+
     return False
 
 def MAIN_CLEAN_BUILD(args):
     set_global(args)
+
     return False
 
 def MAIN(args):
