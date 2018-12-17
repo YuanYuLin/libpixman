6d606b4d93c1b88e862df4191e4902284e3bfd6c
yyl <yuanyu.lin@gmail.com>
2018-12-17 17:29:50 +0800

    MAJOR_NUM=1 MINOR_NUM=5 AUX_NUM=0 update package

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -101,6 +101,18 @@ def MAIN_INSTALL(args):
 
     return False
 
+def MAIN_SDKENV(args):
+    set_global(args)
+
+    cflags = ""
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/' + args["pkg_name"])
+    iopc.add_includes(cflags)
+
+    libs = " -lpixman-1"
+    iopc.add_libs(libs)
+
+    return False
+
 def MAIN_CLEAN_BUILD(args):
     set_global(args)
 
